40b58d733f191da90b34958556f7b8ca80b86573
Austinepass <ohinemeaustine@gmail.com>
2021-09-26 18:19:11 +0100

    Implement Log in flow

--- app/src/main/java/com/orgustine/hagglex/ui/LoginFragment.kt
+++ app/src/main/java/com/orgustine/hagglex/ui/LoginFragment.kt
@@ -1,11 +1,19 @@
 package com.orgustine.hagglex.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.orgustine.apollo.LoginMutation
+import com.orgustine.apollo.type.LoginInput
 import com.orgustine.hagglex.R
 import com.orgustine.hagglex.databinding.FragmentLoginBinding
+import com.orgustine.hagglex.network.Apollo
+import com.orgustine.hagglex.util.AuthStore
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private var _binding: FragmentLoginBinding? = null
@@ -24,6 +32,37 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
     }
 
+    fun login() {
+        if (!validateInput()) {
+
+            val userInput = LoginInput(
+                binding.emailEt.text.toString().trim(),
+                binding.passwordEt.text.toString().trim()
+            )
+            Apollo.apolloClient.mutate(LoginMutation(data = userInput)).enqueue(object :
+                ApolloCall.Callback<LoginMutation.Data>() {
+                override fun onResponse(response: Response<LoginMutation.Data>) {
+                    Log.i("Login Resp", response.toString())
+                    AuthStore.setToken(requireContext(), response.data!!.login.token!!)
+                    findNavController().navigate(R.id.verifyFragment)
+                    findNavController().popBackStack()
+                }
+
+                override fun onFailure(e: ApolloException) {
+                    Log.i("Login ERR", e.toString())
+                }
+            })
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        var failFlag = false
+        if (binding.emailEt.text.toString().trim().isEmpty()) {
+            failFlag = true;
+            binding.emailEt.error = "Invalid code";
+        }
+        return failFlag
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/orgustine/hagglex/ui/MainActivity.kt
+++ app/src/main/java/com/orgustine/hagglex/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.orgustine.hagglex.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.orgustine.hagglex.R
+import com.orgustine.hagglex.util.AuthStore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainAc", AuthStore.getToken(this)!!)
     }
 }
